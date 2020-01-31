32d39ea9225488d9ff159d96abeb1eba793e3876
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-01-31 18:49:03 -0300

    layout complete registeer

--- src/pages/CompleteLogin/index.js
+++ src/pages/CompleteLogin/index.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import {TextInput} from 'react-native';
 
-import {Container, AreaCepNumber, Input} from './styles';
+import {
+  Container,
+  RowBetween,
+  Input,
+  Title,
+  InputSide,
+  SearchAddressButton,
+  SearchAddressButtonText,
+  AreaAddreessResult,
+} from './styles';
 
 export default function CompleteLogin() {
   return (
     <Container>
       <Title>Complete o cadastro para começar:</Title>
-      <AreaCepNumber>
-        <Input
+      <RowBetween style={{marginTop: 16, marginBottom: 16}}>
+        <InputSide
           placeholder="Cep"
           keyboardType="number-pad"
           autoCompleteType="postal-code"
+          maxLength={8}
         />
-        <Input
+        <InputSide
           placeholder="Número"
           keyboardType="number-pad"
           autoCompleteType="off"
+          maxLength={4}
         />
-      </AreaCepNumber>
+      </RowBetween>
 
-      <Input editable={false} placeholder="Endereço" />
+      <SearchAddressButton>
+        <SearchAddressButtonText>Procurar Endereço</SearchAddressButtonText>
+      </SearchAddressButton>
+
+      <AreaAddreessResult>
+        <Input editable={false} placeholder="Endereço" />
+        <Input editable={false} placeholder="Bairro" style={{marginTop: 16}} />
+        <RowBetween>
+          <InputSide
+            editable={false}
+            placeholder="Cidade"
+            style={{marginTop: 16}}
+          />
+          <InputSide
+            editable={false}
+            placeholder="Estado"
+            style={{marginTop: 16}}
+          />
+        </RowBetween>
+      </AreaAddreessResult>
     </Container>
   );
 }
--- src/pages/CompleteLogin/styles.js
+++ src/pages/CompleteLogin/styles.js
@@ -1,7 +1,48 @@
 import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  flex-direction: column;
+  margin: 16px;
+`;
 
-export const AreaCepNumber = styled.View``;
+export const Title = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+`;
 
-export const Input = styled.TextInput``;
+export const RowBetween = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  height: 40px;
+  background-color: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #ccc;
+`;
+
+export const InputSide = styled(Input)`
+  flex-basis: 45%;
+`;
+
+export const SearchAddressButton = styled(RectButton)`
+  margin-bottom: 16px;
+  background-color: #7159c1;
+  padding: 8px 15px;
+  border-radius: 4px;
+`;
+
+export const SearchAddressButtonText = styled.Text`
+  color: #fff;
+  text-align: center;
+`;
+
+export const AreaAddreessResult = styled.View`
+  margin-top: 32px;
+`;
