36f941d9d7a86aab3eb8de4e8ec432cc650b3208
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-02-08 11:03:48 -0300

    dynamic baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = __DEV__ ? 'http://192.168.0.40:3333' : 'http://167.71.179.7';
+
 const api = axios.create({
-  baseURL: 'http://192.168.0.40:3333',
+  baseURL,
 });
 
 export default api;
