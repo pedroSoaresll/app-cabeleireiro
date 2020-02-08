1cd7cf788c9682fcd2fada1d4b3b27d121cce0a1
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-02-08 10:20:06 -0300

    config codepush + onesignal

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import './config/reactotron';
-import React from 'react';
+import React, {Component} from 'react';
 import {StatusBar} from 'react-native';
 import 'react-native-gesture-handler';
 import './config/google-signin';
 import Routes from './routes';
+import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
-export default function App() {
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#333" />
-      <Routes />
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init('cec3a921-28b6-43b3-a2f2-0b70df7d8988');
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => {};
+
+  onOpened = notification => {};
+
+  onIds = id => {};
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="#333" />
+        <Routes />
+      </>
+    );
+  }
 }
+
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(App);
