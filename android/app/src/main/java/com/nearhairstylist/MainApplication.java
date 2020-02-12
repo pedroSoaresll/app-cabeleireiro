8ce9a24ac216d2f2616b8f61343b58ac0c75a2dc
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-02-11 21:51:49 -0300

    fix code push

--- android/app/src/main/java/com/nearhairstylist/MainApplication.java
+++ android/app/src/main/java/com/nearhairstylist/MainApplication.java
@@ -10,7 +10,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -33,11 +32,6 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
-        }
-
-        @Override
-        protected String getJSBundleFile() {
-            return CodePush.getJSBundleFile();
         }
       };
 
